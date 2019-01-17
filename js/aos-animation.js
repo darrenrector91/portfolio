65f3b00280c4a718195044c794db5fdc5c17c744
darrenrector91 <darrenrector91@gmail.com>
2019-01-17 15:33:31 -0600

    initial commit

---
+++ js/aos-animation.js
@@ -0,0 +1,5 @@
+window.onload = function aos_animation_controls() {
+    AOS.init({
+        duration: 1800,
+    });
+}
---
+++ js/scroll-to-top.js
@@ -0,0 +1,13 @@
+$(document).ready( function() {
+    $('#backTop').backTop({
+    
+      // the scroll position in px from the top
+      'position' : 400,
+    
+      // scroll animation speed
+      'speed' : 500,
+    
+      // red, white, black or green
+      'color' : 'red',
+    });
+    });
